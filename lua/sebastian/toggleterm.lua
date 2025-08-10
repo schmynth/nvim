dc43c5a6651268a4b491eefc88d151bd984c3ef7
schmynth <service-noreply@gmx.de>
2025-08-10 23:26:16 +0200

    FIX: terminal toggle

--- lua/sebastian/toggleterm.lua
+++ lua/sebastian/toggleterm.lua
@@ -1,14 +1,14 @@
 local M = {}
 
 require("toggleterm").setup({
-	size = 15,
-	open_mapping = [[<A-d>]], -- Ctrl+t toggles terminal at the bottom
-	direction = "horizontal",
-	shade_terminals = true,
-	start_in_insert = true,
-	insert_mappings = true,
-	persist_size = true,
-	close_on_exit = false,
+  size = 15,
+  open_mapping = [[<A-d>]], -- Ctrl+t toggles terminal at the bottom
+  direction = "horizontal",
+  shade_terminals = true,
+  start_in_insert = true,
+  insert_mappings = true,
+  persist_size = true,
+  close_on_exit = false,
 })
 
 -- Create a persistent terminal instance at the bottom
@@ -16,34 +16,34 @@ local Terminal = require("toggleterm.terminal").Terminal
 
 -- This will be your bottom terminal
 local bottom_term = Terminal:new({
-	direction = "horizontal",
-	hidden = true,
-	close_on_exit = false,
-	on_open = function(term)
-		vim.api.nvim_buf_set_keymap(
-			term.bufnr,
-			"t",
-			"<A-d>",
-			[[<C-\><C-n>:lua require("sebastian.toggleterm").toggle()<CR>]],
-			{ noremap = true, silent = true }
-		)
-	end,
+  direction = "horizontal",
+  hidden = true,
+  close_on_exit = false,
+  on_open = function(term)
+    vim.api.nvim_buf_set_keymap(
+      term.bufnr,
+      "t",
+      "<A-d>",
+      [[<C-\><C-n>:lua require("sebastian.toggleterm").toggle()<CR>]],
+      { noremap = true, silent = true }
+    )
+  end,
 })
 
 function M.toggle()
-	bottom_term:toggle()
+  bottom_term:toggle()
 end
 
 function M.run_build()
-	if not bottom_term:is_open() then
-		bottom_term:toggle() -- open it if not already
-	end
-	bottom_term:send("./buildAndLaunch.sh\n", true)
+  if not bottom_term:is_open() then
+    bottom_term:toggle() -- open it if not already
+  end
+  bottom_term:send("./buildAndLaunch.sh\n", true)
 end
 
 function M.setup_keymaps()
-	vim.keymap.set({ "n", "t" }, "<C-t>", M.toggle, { desc = "Toggle bottom terminal" })
-	vim.keymap.set("n", "<F5>", M.run_build, { desc = "Run buildAndLaunch.sh in terminal" })
+  vim.keymap.set({ "n", "t" }, "<A-d>", M.toggle, { desc = "Toggle bottom terminal" })
+  vim.keymap.set("n", "<F5>", M.run_build, { desc = "Run buildAndLaunch.sh in terminal" })
 end
 
 return M
