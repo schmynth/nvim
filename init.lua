d1dc19fdba56cd0bc367e7a7c44766b4976a032c
schmynth <service-noreply@gmx.de>
2025-08-11 08:52:46 +0200

    REFACTOR: move vim options to lua/config/vim.lua and require it

--- init.lua
+++ init.lua
@@ -1,24 +1,25 @@
 -- disable netrw at the very start of your init.lua
 -- for nvim-tree
 -- vim.g is for global vim settings
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- vim.cmd("set expandtab")
-vim.opt.expandtab = true
-vim.opt.clipboard = "unnamedplus"
-
--- folding: Enable Tree-sitter folding
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldenable = false -- don’t auto-fold by default
-vim.wo.relativenumber = true
-
--- show line numbers
-vim.cmd("set number")
-
--- sets leader character for key mappings
-vim.g.mapleader = " "
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+--
+-- -- vim.cmd("set expandtab")
+-- vim.opt.expandtab = true
+-- vim.opt.clipboard = "unnamedplus"
+--
+-- -- folding: Enable Tree-sitter folding
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+-- vim.opt.foldenable = false -- don’t auto-fold by default
+-- vim.wo.relativenumber = true
+--
+-- -- show line numbers
+-- vim.opt.number = true
+--
+-- -- sets leader character for key mappings
+-- vim.g.mapleader = " "
+require("config.vim")
 
 require("config.lsp")
 require("sebastian.plugins")
@@ -33,9 +34,9 @@ require("sebastian.keymaps")
 
 local config = require("nvim-treesitter.configs")
 config.setup({
-  ensure_installed = { "cpp", "lua", "python", "hyprlang", "css", "csv", "c", "bash" },
-  highlight = { enable = true },
-  indent = { enable = true },
+        ensure_installed = { "cpp", "lua", "python", "hyprlang", "css", "csv", "c", "bash" },
+        highlight = { enable = true },
+        indent = { enable = true },
 })
 
 require("catppuccin").setup()
@@ -45,16 +46,16 @@ vim.cmd.colorscheme("catppuccin")
 vim.lsp.enable({ "clangd", "lua_ls", "pylsp", "bashls" })
 
 require("nvim-tree").setup({
-  sort = {
-    sorter = "case_sensitive",
-  },
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+        sort = {
+                sorter = "case_sensitive",
+        },
+        view = {
+                width = 30,
+        },
+        renderer = {
+                group_empty = true,
+        },
+        filters = {
+                dotfiles = true,
+        },
 })
---
+++ lua/config/vim.lua
@@ -0,0 +1,19 @@
+-- vim.g is for global vim settings
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- vim.cmd("set expandtab")
+vim.opt.expandtab = true
+vim.opt.clipboard = "unnamedplus"
+
+-- folding: Enable Tree-sitter folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldenable = false -- don’t auto-fold by default
+vim.wo.relativenumber = true
+
+-- show line numbers
+vim.opt.number = true
+
+-- sets leader character for key mappings
+vim.g.mapleader = " "
