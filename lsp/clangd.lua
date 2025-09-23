26ff603979153e17fcbd1fb77a854da3d803902c
schmynth <service-noreply@gmx.de>
2025-09-23 15:40:04 +0200

    ENABLE: rust lsp

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -1,6 +1,6 @@
 return {
-  cmd = { 'clangd', '--background-index', '--compile-commands-dir=build' },
-  filetypes = { 'cpp', 'h', 'c' },
-  -- root_markers are needed for lsp to work
-  root_markers = { '.git', 'buildAndLaunch.sh' }
+    cmd = { "clangd", "--background-index", "--compile-commands-dir=build" },
+    filetypes = { "cpp", "h", "c" },
+    -- root_markers are needed for lsp to work
+    root_markers = { ".git", "buildAndLaunch.sh" },
 }
