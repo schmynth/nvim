e37be5e894a1f32260a8b6671849d68c3800e73e
schmynth <service-noreply@gmx.de>
2025-10-21 14:06:57 +0200

    ADD: markdown TOC plugin

--- lua/sebastian/plugins.lua
+++ lua/sebastian/plugins.lua
@@ -86,6 +86,22 @@ vim.list_extend(plugins, {
         dependencies = {},
     },
 
+    {
+        "ChuufMaster/markdown-toc",
+        opts = {
+
+            -- The heading level to match (i.e the number of "#"s to match to) max 6
+            heading_level_to_match = -1,
+
+            -- Set to True display a dropdown to allow you to select the heading level
+            ask_for_heading_level = false,
+
+            -- TOC default string
+            -- WARNING: it only displays correctly on github, if not indented!
+            toc_format = "%s- [%s](%s#%s)",
+        },
+    },
+
     { -- Autocompletion
         "saghen/blink.cmp",
         event = "VimEnter",
